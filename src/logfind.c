d88e2607baf204eebbb16757da36191f19d01a2f
Lilith Tundrus <echofox123@gmail.com>
2018-05-19 19:49:32 -0400

    Okay I hate C a little less but pointers are hard :c

--- src/logfind.c
+++ src/logfind.c
@@ -18,6 +18,12 @@ void die(const char *message)
     }
 }
 
+void help() {
+    char help_message[] = 
+    "USAGE: \n\nlogfind [argument] string, string ...\n\n[argument] can be -o, -a or -h\n";
+    printf(help_message);
+}
+
 int main(int argc, char *argv[])
 {
     log_info("LogFind %s starting...", version);
@@ -29,14 +35,23 @@ int main(int argc, char *argv[])
         die(err_msg);
     }
 
+    // iterate over each argument, stuffing the 3rd+ args into an 
+    // array to check a set of logs for
     for (int i = 0; i < argc; i++)
     {
         printf("%s\n", argv[i]);
         if (strstr(argv[i], "-o"))
         {
             log_info("Switching to OR mode");
-        } else {
-            // do nothing
+        }
+        else if (strstr(argv[i], "-h"))
+        {
+            help();
+            break;
+        }
+        else
+        {
+            // do nothing, we've already set the mode
         }
     }
 
