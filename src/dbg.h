8f83e6601c4e3272727ef9275e0120aea67a05db
Lilith Tundrus <echofox123@gmail.com>
2018-05-19 19:21:58 -0400

    Added ASCII/ANSI colors from another example page, formatted document

--- src/dbg.h
+++ src/dbg.h
@@ -5,35 +5,59 @@
 #include <errno.h>
 #include <string.h>
 
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_GREY "\x1b[30m"
+#define ANSI_COLOR_LPURPLE "\x1b[35m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
-        __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", \
+                              __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr,\
-        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-        clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr,                                                   \
+                                "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
+                                clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr,\
-        "[WARN] (%s:%d: errno: %s) " M "\n",\
-        __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr,                              \
+                                 "[WARN] (%s:%d: errno: %s) " M "\n", \
+                                 __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
-        __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", \
+                                 __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define check(A, M, ...) if(!(A)) {\
-    log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define check(A, M, ...)                   \
+        if (!(A))                          \
+        {                                  \
+                log_err(M, ##__VA_ARGS__); \
+                errno = 0;                 \
+                goto error;                \
+        }
 
-#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__);\
-    errno=0; goto error; }
+#define sentinel(M, ...)                   \
+        {                                  \
+                log_err(M, ##__VA_ARGS__); \
+                errno = 0;                 \
+                goto error;                \
+        }
 
 #define check_mem(A) check((A), "Out of memory.")
 
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__);\
-    errno=0; goto error; }
+#define check_debug(A, M, ...)           \
+        if (!(A))                        \
+        {                                \
+                debug(M, ##__VA_ARGS__); \
+                errno = 0;               \
+                goto error;              \
+        }
 
 #endif
